4255111996fcdc19784af724e3e94d10f67d8639
huseyinkarabas <you@example.com>
2023-05-23 21:39:28 +0200

    18. gün sonu

--- src/test/java/tests/day18_TestNGReports_ParalelCalistirma/C01_RaporluAmazonSearch.java
+++ src/test/java/tests/day18_TestNGReports_ParalelCalistirma/C01_RaporluAmazonSearch.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import pages.AmazonPage;
 import utilities.ConfigReader;
 import utilities.Driver;
+import utilities.ReusableMethods;
 import utilities.TestBaseRapor;
 public class C01_RaporluAmazonSearch extends TestBaseRapor {
     @Test
@@ -15,17 +16,20 @@ public class C01_RaporluAmazonSearch extends TestBaseRapor {
       //amazon'a gidin
         Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
         extentTest.info("Kullanici Amazon anasayfaya gider");
+        ReusableMethods.bekle(2);
 
       //Nutella icin arama yapin
         AmazonPage amazonPage=new AmazonPage();
         amazonPage.aramaKutusu.sendKeys(ConfigReader.getProperty("amazonAranacakKelime")+ Keys.ENTER);
         extentTest.info("Arama kutusuna belirlenen aranacak kelimeyi yazar ve aratır");
+        ReusableMethods.bekle(2);
 
       //arama sonuclarinin Nutella icerdigini test edin
 
         String actualSonucYazisi=amazonPage.aramaSonucuElementi.getText();
         String expectedIcerik=ConfigReader.getProperty("amazonExpectedIcerik");
         extentTest.info("Arama sonucları kaydedildi");
+        ReusableMethods.bekle(2);
 
         Assert.assertTrue(actualSonucYazisi.contains(expectedIcerik));
         extentTest.pass("belirlenen aranacak kelime aratıldıgında , arama sonucunda expected icerik oldugu test edildi");
